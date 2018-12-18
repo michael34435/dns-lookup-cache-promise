6a1971fcedcfdf454f1128a9a4c6b7b3f4f9c371
Michael.K <michael34435@capslock.tw>
2018-12-18 13:52:23 +0800

    feat: first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const lookup = require('dns-lookup-cache');
+
+module.exports = (domain, options) => {
+  const promise = new Promise((resolve, reject) => {
+    lookup(
+      domain,
+      options,
+      (error, address, family) => {
+        if (error) {
+          return reject(error);
+        }
+
+        return resolve({ address, family });
+      },
+    );
+  });
+
+  return promise;
+};
